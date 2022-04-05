9d878b05cceb0233fdcb40ca8f245d2218c9de8f
yunusp <yunuspoonawala52@gmail.com>
2022-04-06 01:25:35 +0530

    animated player WIP

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Super Class of all Entities: players, npcs, etc.
  */
@@ -7,5 +9,9 @@ public class Entity {
     public int x,y;
     public int speed;
 
+    public BufferedImage up1, up2, down1, down2, right1, right2, left1, left2;
+    public String direction;
 
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,11 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -14,23 +18,83 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
-        if (keyH.upPressed) y -= speed;
-        else if (keyH.downPressed) y += speed;
-        else if (keyH.leftPressed) x -= speed;
-        else if (keyH.rightPressed) x += speed;
+        if (keyH.upPressed) {
+            direction = "up";
+            y -= speed;
+        } else if (keyH.downPressed) {
+            direction = "down";
+            y += speed;
+        } else if (keyH.leftPressed) {
+            direction = "left";
+            x -= speed;
+        } else if (keyH.rightPressed) {
+            direction = "right";
+            x += speed;
+        }
+
+        spriteCounter++;
+        if (spriteCounter > 10) {
+            if (spriteNum == 1) spriteNum = 2;
+            else if (spriteNum == 2) spriteNum = 1;
+            spriteCounter = 0;
+        }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up" -> {
+                if (spriteNum == 1)
+                    image = up1;
+                if (spriteNum == 2)
+                    image = up2;
+            }
+            case "down" -> {
+                if (spriteNum == 1)
+                    image = down1;
+                if (spriteNum == 2)
+                    image = down2;
+            }
+            case "right" -> {
+                if (spriteNum == 1)
+                    image = right1;
+                if (spriteNum == 2)
+                    image = right2;
+            }
+            case "left" -> {
+                if (spriteNum == 1)
+                    image = left1;
+                if (spriteNum == 2)
+                    image = left2;
+            }
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
