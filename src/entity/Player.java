776a632948d97f8b492b1fb9ae8c54ecae545e08
yunusp <yunuspoonawala52@gmail.com>
2022-04-07 01:30:58 +0530

    added final screen

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -110,7 +110,7 @@ public class Player extends Entity {
                         gp.playSE(3);
                         hasKey--;
                         gp.ui.showMessage("You opened a door");
-                    }else {
+                    } else {
                         gp.ui.showMessage("You need a key");
                     }
                     break;
@@ -123,7 +123,9 @@ public class Player extends Entity {
                     gp.obj[i] = null;
                     break;
                 case "Chest":
-
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + objectName);
--- src/main/UI.java
+++ src/main/UI.java
@@ -7,18 +7,21 @@ import java.awt.image.BufferedImage;
 
 public class UI {
     GamePanel gp;
-    Font arial_40;
+    Font arial_40, arial_80B;
     BufferedImage keyImage;
 
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
 
+    public boolean gameFinished = false;
+
 
     public UI(GamePanel gp) {
         this.gp = gp;
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+        arial_80B = new Font("Arial", Font.BOLD, 80);
 
         OBJ_Key key = new OBJ_Key();
         keyImage = key.image;
@@ -31,21 +34,50 @@ public class UI {
 
     public void draw(Graphics2D g2) {
 
-        g2.setFont(arial_40);
-        g2.setColor(Color.WHITE);
-        g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
-        g2.drawString("x " + gp.player.hasKey, 74, 65);
+        if (gameFinished) {
 
-        //MESSAGE
-        if (messageOn) {
-            g2.setFont(g2.getFont().deriveFont(30F));
-            g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
+            String text;
+            int textLength;
+            int x, y;
 
-            messageCounter++;
+            g2.setFont(arial_40);
+            g2.setColor(Color.WHITE);
+            text = "You Found the Treasure";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 
-            if(messageCounter > 120) {
-                messageCounter = 0;
-                messageOn = false;
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 - gp.tileSize * 3;
+            g2.drawString(text, x, y);
+
+            g2.setFont(arial_80B);
+            g2.setColor(Color.YELLOW);
+            text = "Congratulations";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 - gp.tileSize * -4;
+            g2.drawString(text, x, y);
+
+            gp.gameThread = null;
+
+        } else {
+
+            g2.setFont(arial_40);
+            g2.setColor(Color.WHITE);
+            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
+            g2.drawString("x " + gp.player.hasKey, 74, 65);
+
+            //MESSAGE
+            if (messageOn) {
+                g2.setFont(g2.getFont().deriveFont(30F));
+                g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
+
+                messageCounter++;
+
+                if (messageCounter > 120) {
+                    messageCounter = 0;
+                    messageOn = false;
+                }
             }
         }
     }
