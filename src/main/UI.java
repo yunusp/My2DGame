d5772123487725318ba7db5c4314299eece51a10
yunusp <yunuspoonawala52@gmail.com>
2022-04-07 02:19:34 +0530

    added speedrun time

--- src/main/UI.java
+++ src/main/UI.java
@@ -4,6 +4,7 @@ import object.OBJ_Key;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 public class UI {
     GamePanel gp;
@@ -15,6 +16,9 @@ public class UI {
     int messageCounter = 0;
 
     public boolean gameFinished = false;
+
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
 
 
     public UI(GamePanel gp) {
@@ -44,21 +48,26 @@ public class UI {
             g2.setColor(Color.WHITE);
             text = "You Found the Treasure";
             textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-
             x = gp.screenWidth / 2 - textLength / 2;
             y = gp.screenHeight / 2 - gp.tileSize * 3;
+            g2.drawString(text, x, y);
+
+            text = "Your time: " + dFormat.format(playTime) + "s!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 + gp.tileSize * 2;
             g2.drawString(text, x, y);
 
             g2.setFont(arial_80B);
             g2.setColor(Color.YELLOW);
             text = "Congratulations";
             textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-
             x = gp.screenWidth / 2 - textLength / 2;
             y = gp.screenHeight / 2 - gp.tileSize * -4;
             g2.drawString(text, x, y);
 
             gp.gameThread = null;
+
 
         } else {
 
@@ -66,6 +75,10 @@ public class UI {
             g2.setColor(Color.WHITE);
             g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
             g2.drawString("x " + gp.player.hasKey, 74, 65);
+
+            //Time
+            playTime += (double) 1 / 60;
+            g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize * 11, 65);
 
             //MESSAGE
             if (messageOn) {
